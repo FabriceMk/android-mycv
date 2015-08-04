a9860eb00c7bd5b97054b5046a5142fd92559323
FabriceMk <fabricemk@users.noreply.github.com>
2015-08-04 22:59:15 +0900

    Adjust SkillDetails layout. Up arrow now working as Back

--- android/src/main/java/net/fabricemk/android/mycv/ui/activities/SkillDetailsActivity.java
+++ android/src/main/java/net/fabricemk/android/mycv/ui/activities/SkillDetailsActivity.java
@@ -1,6 +1,8 @@
 package net.fabricemk.android.mycv.ui.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -8,13 +10,20 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.transition.Slide;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.Target;
 
 import net.fabricemk.android.mycv.R;
 import net.fabricemk.android.mycv.adapters.SkillListAdapter;
@@ -53,6 +62,7 @@ public class SkillDetailsActivity extends AppCompatActivity {
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
         String itemTitle = getIntent().getStringExtra(EXTRA_TITLE);
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle(itemTitle);
@@ -63,6 +73,7 @@ public class SkillDetailsActivity extends AppCompatActivity {
         TextView descriptionView = (TextView) findViewById(R.id.description);
 
         int resourceId = SkillListAdapter.mappingIconIdFromName(getIntent().getStringExtra(EXTRA_ICON));
+
         Glide.with(this)
                 .load(resourceId)
                 .centerCrop()
@@ -84,5 +95,28 @@ public class SkillDetailsActivity extends AppCompatActivity {
             getWindow().setReturnTransition(transition);
         }
     }
+
+    private void applyPalette(Palette palette) {
+        int primaryDark = getResources().getColor(R.color.primary_dark);
+        int primary = getResources().getColor(R.color.primary);
+        collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
+        collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
+        //supportStartPostponedEnterTransition();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle item selection
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
 }
