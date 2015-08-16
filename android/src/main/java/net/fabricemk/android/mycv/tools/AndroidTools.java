315ca9b95368c0ce8f5e825dc80e72a36912c4e0
FabriceMk <fabricemk@users.noreply.github.com>
2015-08-16 23:03:25 +0900

    Prepare tools for networking

--- android/src/main/java/net/fabricemk/android/mycv/tools/AndroidTools.java
+++ android/src/main/java/net/fabricemk/android/mycv/tools/AndroidTools.java
@@ -1,8 +1,17 @@
 package net.fabricemk.android.mycv.tools;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 public class AndroidTools {
+
+    public static boolean isOnline(Context ctxt) {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                ctxt.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
 
     public static int getDrawableIdFromName(Context ctxt, String drawableName) {
         return ctxt.getResources().getIdentifier(drawableName , "drawable", ctxt.getPackageName());
