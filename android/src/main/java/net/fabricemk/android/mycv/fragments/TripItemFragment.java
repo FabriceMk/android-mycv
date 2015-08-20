a51a994fc3b955c34077ee0a6f3c63d3b8bd41af
FabriceMk <fabricemk@users.noreply.github.com>
2015-08-20 22:03:58 +0900

    Begin work on GoogleMaps implementation

--- android/src/main/java/net/fabricemk/android/mycv/fragments/TripItemFragment.java
+++ android/src/main/java/net/fabricemk/android/mycv/fragments/TripItemFragment.java
@@ -2,13 +2,26 @@ package net.fabricemk.android.mycv.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import net.fabricemk.android.mycv.R;
 
-public class TripItemFragment extends Fragment {
+public class TripItemFragment extends Fragment implements OnMapReadyCallback {
+
+    SupportMapFragment mSupportMapFragment;
+
+    GoogleMap googleMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,7 +34,47 @@ public class TripItemFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.trip_item, container, false);
 
+
+        mSupportMapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        if (mSupportMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mSupportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.map, mSupportMapFragment).commit();
+        }
+        if (mSupportMapFragment != null) {
+            googleMap = mSupportMapFragment.getMap();
+            if (googleMap != null) {
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng point) {
+                        //TODO: your onclick stuffs
+                    }
+                });
+            }
+
+        }
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
+
         return v;
     }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+
 
 }
